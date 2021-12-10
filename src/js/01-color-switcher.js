fad13fd313a10e919e1ab525b792619ea0b2293b
Igor Makojda <imakojda@gmail.com>
2021-12-10 22:54:56 +0200

    task 3 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,20 +1,27 @@
 const btnStart=document.querySelector(`[data-start]`);
-console.log(btnStart);
+
 const btnStop=document.querySelector(`[data-stop]`);
-console.log(btnStart);
 
 btnStart.addEventListener('click',()=>{
+
     timerId = setInterval(() => {
         document.body.style.backgroundColor = getRandomHexColor();
     }, 1000);
+
     btnStart.setAttribute('disabled', true);
-    console.log("Button was clicked");});
+});
+
 btnStop.addEventListener('click',()=>{
+
     clearInterval(timerId);
+
     btnStart.removeAttribute('disabled');
-    console.log("Button was clicked");});
+
+});
 
 function getRandomHexColor() {
+
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
   }
   
